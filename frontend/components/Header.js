21f3489dd8ab21f3fc33292641752a887f2f55ab
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-20 10:52:13 -0400

    Add global styles, typography

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -20,7 +20,7 @@ const Logo = styled.h1`
 
 const HeaderStyles = styled.header`
     .bar {
-        border-bottom: 10px solid var(--black, black);
+        border-bottom: 10px solid var(--grey, black);
         display: grid;
         grid-template-columns: auto 1fr;
         justify-content: space-between;
@@ -30,7 +30,7 @@ const HeaderStyles = styled.header`
     .sub-bar {
         display: grid;
         grid-template-columns: 1fr auto;
-        border-bottom: 10px solid var(--black, black);
+        border-bottom: 10px solid var(--grey, black);
     }
 `;
 
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,10 +1,64 @@
 import Header from "./Header"
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2');
+        format('woff2')
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    :root {
+        --pink: #ff1493;
+        --black: #393939;
+        --grey: #708090;
+        --lightGrey: #e1e1e1;
+        --offWhite: #ededed;
+        --maxWidth: 1000px;
+        --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+        box-sizing: border-box;
+    }
+
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding: 0;
+        margin: 0;
+        font-size: 1rem;
+        line-height:1;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--black);
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    button {
+        font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`; 
 
 export default function Page({children}) {
     return(
         <div>
+            <GlobalStyles />
             <Header />
-            {children}
+            <InnerStyles>{children}</InnerStyles>
         </div>
     )
 }
