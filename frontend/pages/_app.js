4a2c501eaa386f87881e0d5386f727fce7d52c10
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-24 10:50:53 -0400

    Match css styles between the serve and client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,6 @@
 import Page from "../components/Page";
 import NProgress from 'nprogress';
 import Router from 'next/router';
-//import 'nprogress/nprogress.css';
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
