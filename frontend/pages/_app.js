59f5dc67ec8b5d7094c311d6404cae79db0c7709
Vadim Geroim <vgeroim.qa@gmail.com>
2024-09-24 09:47:24 -0400

    Add spinner on route changes with nprogress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,12 @@
-import Page from "../components/Page"
+import Page from "../components/Page";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+//import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({Component, pageProps}) {
     return(
